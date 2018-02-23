8c10461dce37793cca6ad33ca2bf089e1fa28a45
Mingyun KIM <mikim@student.42.us.org>
2018-02-22 21:19:01 -0800

    Piscine PHP: delete unnecessary part

--- day/d02/ex02/magnifying_glass.php
+++ day/d02/ex02/magnifying_glass.php
@@ -12,11 +12,11 @@ $write = preg_replace_callback("/(<a )(.*?)(>)(.*)(<\/a>)/si", function($match)
 {
 	$match[0] = preg_replace_callback("/( title=\")(.*?)(\")/mi", function($match)
    	{
-		return ($match[1]."".strtoupper($match[2])."".$match[3]);
+		return ($match[1].strtoupper($match[2])."".$match[3]);
 	}, $match[0]);
 	$match[0] = preg_replace_callback("/(>)(.*?)(<)/si", function($match)
    	{
-		return ($match[1]."".strtoupper($match[2])."".$match[3]);
+		return ($match[1].strtoupper($match[2])."".$match[3]);
 	}, $match[0]);
 	return ($match[0]);
 }, $write);
